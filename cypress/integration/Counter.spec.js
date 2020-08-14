2896a2aa44cf3cfd7f5adf95ada32abbc84ac829
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-14 13:03:21 +0200

    set up cypress

---
+++ cypress/integration/Counter.spec.js
@@ -0,0 +1,10 @@
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:1234 ')
+  })
+
+  it('has the proper heading', () => {
+    cy.get('.widget-counter h2')
+      .should('have.text', 'Counter')
+  })
+})
