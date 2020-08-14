9fa524f4519bd9c456c4f817a8184253a58e82c8
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-14 16:18:39 +0200

    passing tests

--- cypress/integration/Counter.spec.js
+++ cypress/integration/Counter.spec.js
@@ -5,6 +5,15 @@ context('Actions', () => {
 
   it('has the proper heading', () => {
     cy.get('.widget-counter h2')
-      .should('have.text', 'CounteRxx')
+      .should('have.text', 'Counter')
+  })
+  it('has an increment button', () => {
+    cy.contains('Increment')
+  })
+  it('has an decrement button', () => {
+    cy.contains('Decrement')
+  })
+  it('has a reset button', () => {
+    cy.contains('Reset')
   })
 })
